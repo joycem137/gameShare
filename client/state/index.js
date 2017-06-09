e15018088b0ef6f4e3afa56702cf5fbfcb4f643d
Joyce Mayorga <joycem137@gmail.com>
2017-06-09 03:13:15 -0700

    Added row B

--- client/state/index.js
+++ client/state/index.js
@@ -6,8 +6,10 @@ class State {
         this.pieces = [
             {id: 0, color: 'white', height: 1, location: 'A1'},
             {id: 1, color: 'black', height: 1, location: 'A2'},
-            {id: 2, color: 'white', height: 2, location: 'A3'},
+            {id: 2, color: 'white', height: 2, location: 'B3'},
             {id: 3, color: 'black', height: 2, location: 'A5'},
+            {id: 4, color: 'black', height: 2, location: 'B1'},
+            {id: 5, color: 'black', height: 1, location: 'B2'},
         ];
     }
     getState() {
