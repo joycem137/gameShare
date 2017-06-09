036ce609c27d9436fb09db90f78cf1694111e236
Joyce Mayorga <joycem137@gmail.com>
2017-06-09 03:35:35 -0700

    Finished board.

--- client/state/index.js
+++ client/state/index.js
@@ -5,11 +5,17 @@ class State {
     constructor() {
         this.pieces = [
             {id: 0, color: 'white', height: 1, location: 'A1'},
-            {id: 1, color: 'black', height: 1, location: 'A2'},
+            {id: 1, color: 'black', height: 1, location: 'I2'},
             {id: 2, color: 'white', height: 2, location: 'B3'},
             {id: 3, color: 'black', height: 2, location: 'A5'},
-            {id: 4, color: 'black', height: 2, location: 'B1'},
+            {id: 4, color: 'black', height: 2, location: 'H3'},
             {id: 5, color: 'black', height: 1, location: 'B2'},
+            {id: 6, color: 'white', height: 2, location: 'C5'},
+            {id: 7, color: 'black', height: 2, location: 'E1'},
+            {id: 8, color: 'white', height: 1, location: 'F2'},
+            {id: 9, color: 'white', height: 1, location: 'D5'},
+            {id: 10, color: 'black', height: 1, location: 'D3'},
+            {id: 11, color: 'white', height: 1, location: 'D2'},
         ];
     }
     getState() {
